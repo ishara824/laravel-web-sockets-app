4a53aa2b1305af7264c654aaf854730758d9664a
Ishara_Miyushan <ishara824@gmail.com>
2025-01-06 08:01:17 +0530

    integrate pusher beams

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Events\PostNotification;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Pusher\PushNotifications\PushNotifications;
 
 class PostController extends Controller
 {
@@ -23,10 +24,27 @@ class PostController extends Controller
         ]);
 
         // Dispatch the event with the post data
-        broadcast(new PostNotification([
-            'author' => $post->author,
-            'title' => $post->title,
-        ]));
+        // broadcast(new PostNotification([
+        //     'author' => $post->author,
+        //     'title' => $post->title,
+        // ]));
+
+        $beamsClient = new PushNotifications([
+            'instanceId' => env('VITE_PUSHER_BEAMS_INSTANCE_ID'),
+            'secretKey' => env('VITE_PUSHER_BEAMS_SECRET_KEY')
+        ]);
+
+        $publishResponse = $beamsClient->publishToInterests(
+            ["hello"],
+            [
+                "web" => [
+                    "notification" => [
+                        "title" => $post->author,
+                        "body" => $post->title
+                    ]
+                ]
+            ]
+        );
 
         // Redirect with success message
         return redirect()->back()->with('success', 'Post created successfully!');
